d8aaf326fd96fd73f56ae536f2d4bb60899c5e56
MuhammadAmin4O <mhmmdamin357@gmail.com>
2023-06-25 16:56:24 +0800

    upload ketiga tabel ortu

--- laporan_siswa.dpr
+++ laporan_siswa.dpr
@@ -3,12 +3,14 @@ program laporan_siswa;
 uses
   Forms,
   Tabelkelas in 'Tabelkelas.pas' {Form1},
-  Tabelwalikelas in 'Tabelwalikelas.pas' {Form2};
+  Tabelwalikelas in 'Tabelwalikelas.pas' {Form2},
+  Tabelortu in 'Tabelortu.pas' {Form3};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TForm3, Form3);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm1, Form1);
   Application.Run;
