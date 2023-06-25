39be5f52c22398213adcd8b056901423f073bf82
MuhammadAmin4O <mhmmdamin357@gmail.com>
2023-06-25 22:09:57 +0800

    upload kelima tabel user

--- laporan_siswa.dpr
+++ laporan_siswa.dpr
@@ -5,12 +5,14 @@ uses
   Tabelkelas in 'Tabelkelas.pas' {Form1},
   Tabelwalikelas in 'Tabelwalikelas.pas' {Form2},
   Tabelortu in 'Tabelortu.pas' {Form3},
-  Tabelpoin in 'Tabelpoin.pas' {Form4};
+  Tabelpoin in 'Tabelpoin.pas' {Form4},
+  Tabeluser in 'Tabeluser.pas' {Form5};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm4, Form4);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm3, Form3);
